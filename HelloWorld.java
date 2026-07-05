5be0057ea1c1efbc1a6824af057ef80c7c11baa9
Eduardo Ramirez Camacho <eduramcam@hotmail.com>
2026-07-05 04:17:16 -0600

    Add changes from Intell IJ

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World para probar Action 2!");
         System.out.println("Aqui ya agregamos dos lineas de codigo ");
+        System.out.println("Este es un cambio efectuado desde intellIJ ");
     }
 }
