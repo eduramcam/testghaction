ef562117e2ca727fa67e702245e60674e26e1ae0
Eduardo Ramirez Camacho <eduramcam@hotmail.com>
2026-07-05 04:32:55 -0600

    Se agrega un codigo que no compila

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Se eliminan los otros
+        System.out.println("Se eliminan los otros prints ");
 
     }
 }
