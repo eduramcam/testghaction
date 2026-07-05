ac9480bc45aee7ee3ff61cde6e5843d043e81398
Eduardo Ramirez Camacho <eduramcam@hotmail.com>
2026-07-05 04:08:05 -0600

    se agrega al pull request del nuevo cambio

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World para probar Action 2!");
+        System.out.println("Aqui ya agregamos dos lineas de codigo ");
     }
 }
